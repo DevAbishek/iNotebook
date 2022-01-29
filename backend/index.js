a58414162e1bd9d09c73707d15fe33de831b1503
Abishek Agarwal <disk.abishek@gmail.com>
2022-01-29 13:32:04 +0530

    vid 45 completed

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const connectToMongo = require('./db');
 connectToMongo();
 
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Abishek!')
-})
+app.use(express.json());
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
